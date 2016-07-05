0f9c8196b617f8c7066153f1861f4cfb00307728
Johnny Pacia <johnny.pacia@gmail.com>
2016-07-05 13:52:41 -0400

    adds boss rev logo to presentation screen

--- BossRevPay/App/Containers/PresentationScreen.js
+++ BossRevPay/App/Containers/PresentationScreen.js
@@ -26,7 +26,7 @@ class PresentationScreen extends React.Component {
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
           <View style={styles.centered}>
-            <Image source={Images.clearLogo} style={styles.logo} />
+            <Image source={Images.bossRevpayFlat} style={styles.logo} />
           </View>
 
           <View style={styles.section} >
